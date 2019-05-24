a5624f893fa2a9c86524d866cdebf6dc681fd34d
Bethoth <45798269+Bethoth@users.noreply.github.com>
2019-05-24 16:08:21 +0200

    Update index.js

--- index.js
+++ index.js
@@ -1,29 +1,38 @@
-const discord = require("discord.js");
-const {translate, detectLanguage, wordAlternatives, translateWithAlternatives} = require("deepl-translator");
+const discord = require(`discord.js`);
+const translate = require('@k3rn31p4nic/google-translate-api');
 
 const client = new discord.Client();
 
-const constants = require("./constants");
-const embeds = require("./embeds");
+const constants = require(`./constants`);
+const embeds = require(`./embeds`);
+const utilFunctions = require(`./utilFunctions`);
 
-client.on("ready", () => {
-    console.log("I'm ready !");
-    client.user.setActivity("Préfixe : " + constants.lPrefix);
+client.on(`ready`, () => {
+    console.log(`I'm ready !`);
+    client.user.setActivity(`Préfixe : ` + constants.lPrefix);
 });
 
-client.on("message", (message) => {
+client.on(`message`, (message) => {
     if(message.content.startsWith(constants.lPrefix) || message.content.startsWith(constants.uPrefix)) {
-        const args = message.content.slice(constants.lPrefix.length).trim().split(";");
+        const args = message.content.slice(constants.lPrefix.length).trim().split(` `);
         const command = args.shift().toLowerCase();
 
-        if(command === "invite") {
+        if(command === `invite`) {
             message.channel.send(embeds.invite);
         }
 
-        else if(command === "translate") {
-            const stringToTranslate = args[0];
+        else if(command === "t") {
+            const targetLanguage = args[0];
             const sourceLanguage = args[1];
-            const targetLanguage = args[2];
+            const word = args[2];
+            
+            translate(word, {from: sourceLanguage, to: targetLanguage})
+            .then(res => message.channel.send(utilFunctions.result(word, res.text, sourceLanguage, targetLanguage)))
+            .catch(err => console.error(err));
+        }
+
+        else if(command === "list") {
+            message.channel.send(embeds.languagesLists);
         }
     }
-})
+});
